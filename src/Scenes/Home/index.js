ae331f379f653909de00592521fce8cd01d87ab6
vinicius97 <contato@viniciuslana.com>
2018-07-10 22:27:35 -0300

    Implementar cenas ausentes das rotas
    Foram implementadas as cenas que estavam ausentes sendo elas
    Home, NotFound e VideoPlayer

---
+++ src/Scenes/Home/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import './index.scss'
+
+export class Home extends Component{
+
+  render(){
+    return(
+      <div>
+        Home
+      </div>
+    )
+  }
+}
+
+export default Home
---
+++ src/Scenes/VideoPlayer/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class VideoPlayer extends Component{
+  render(){
+    return <div>
+      Vídeo
+    </div>
+  }
+}
+
+export default VideoPlayer
